326e1634aa32a16406d79a8b6749e50bd920b4e4
hrecht <hrecht@users.noreply.github.com>
2015-12-29 15:58:21 -0500

    update text

--- js/text.js
+++ js/text.js
@@ -16,24 +16,23 @@ var catname = {
 
 //text for levels of categories - don't bother with the csvs! make all edits here
 var levels = {
-    main: ["Share of all children who are COI", "Total number", "Percent of national total"],
+    main: ["Share of all children with immigrant parents", "Total number", "Children of immigrants by residence"],
     age: ["Age 0 to 3", "Age 4 to 5", "Age 6 to 12", "Age 13 to 17"],
-    origin: ["Mexico", "Other Central America & Spanish Caribbean", "Europe, Canada & Australia", "East Asia & Pacific", "Africa", "The Middle East & South Asia", "South America", "Southeast Asia"],
+    origin: ["Mexico", "Other Central America & Spanish Caribbean", "Europe, Canada, & Australia", "East Asia & Pacific", "Africa", "The Middle East & South Asia", "South America", "Southeast Asia"],
     citizenship: ["Citizen with citizen parents", "Citizen with non-citizen parents", "Non-citizen"],
     edukids: ["Not in school, age 3 to 5", "Not in school, age 6 to 17"],
-    edupar: ["Less than a high school degree", "High school degree", "At least four year college degree"],
-    engkids: ["English Proficient", "With Limited English Proficiency"],
-    engpar: ["English proficient parents", "At least one Limited English Proficient parent", "No English proficient parents", "Who live in Linguistically Isolated Households"],
+    edupar: ["No high school degree", "High school degree", "Bachelor's degree or more"],
+    engkids: ["English proficient", "Not English proficient"],
+    engpar: ["English proficient parents", "At least one parent who is not English proficient", "No English proficient parents", "Linguistically isolated households"],
     numkids: ["1", "2", "3-4", "5+"],
-    numpar: ["Single parent", "Two parent"],
-    incbenefits: ["In families below 100% of the poverty line", "In families below 200% of the poverty line",
-"Working family", "In low income working family", "Household owns home"]
+    numpar: ["One parent", "Two parents"],
+    incbenefits: ["Family below the poverty line", "Family below 200% of the poverty line", "At least one working parent", "At least one working parent and below poverty line", "Household owns home"]
 };
 
 //sumamary text for each category
 var cattext = {
     main: "From 2011 to 2013, the number of children of immigrants grew from NUMBER to NUMBER. As before, these children continue to be clustered in six states that are traditional immigrant destinations: California, New York, New Jersey, Florida, Illinois, and Texas. Yet, percentage of children of immigrants in California has declined from 2006 to 2013 and some nontraditional states, such as Kansas and South Carolina, saw an increase in the share of children of immigrants.",
-    age: "Share of children that are children of immigrants in a certain age group.",
+    age: "Share of children in a certain age group that are children of immigrants.",
     origin: "Country of origin is particularly important from a policy perspective, as it influences the types of language accessibility programs necessary to serve students and their parents in a particular area. Nationwide, Mexico was the most common country of origin, but many of these immigrants cluster in Western states. In Texas, for example, 69 percent of immigrant parents came from Mexico. In New York, however, another traditional immigrant state, only 9 percent of children of immigrants had parents born in Mexico. In fact, there was no clear majority in New York; the largest share of immigrant parents came from Central America and the Spanish Caribbean, but still only accounted for 20 percent of the immigrant parent population.",
     citizenship: "Because citizenship often determines eligibility for federal programs aimed at low-income families, understanding the distribution of citizenship among children of immigrants is vital for making decisions about supplemental state programs for non-citizens. The share of children of immigrants who are citizens with citizen parents increased from 55 percent in 2006 to 57 percent in 2013. Yet, the share of children of immigrants who are citizens with non-citizen parents also increased, from 31 percent in 2006 to 33 percent in 2013. Non-citizen parents are less likely to participate in programs like SNAP or TANF even when their citizen children are eligible.",
     edukids: "School participation rates for children of immigrants have been increasing since 2006. Nearly all children of immigrants ages 6 to 17 are in school—just 1.75 percent are not, a smaller share than that of children of native born parents who are not in school. But for younger children, the story is different. Nationwide, just 59.3 percent of children of immigrants attend school, and the school-going rate is lower than for children of native born parents.",
